b09367305595c92bdba925ce49766cf4d05429af
Sohail Kachhi <43206109+sohail-iirnt@users.noreply.github.com>
2026-08-22 18:19:53 +0530

    feat(tasks): finalize task input model

--- src/types/task.ts
+++ src/types/task.ts
@@ -20,15 +20,22 @@ export type Task = BaseEntity & {
   projectId?: ID | null
   clientId?: ID | null
   assigneeId?: ID | null
-
   title: string
   description?: string
-
   status: TaskStatus
   priority: TaskPriority
-
   dueDate?: Date | null
   completedAt?: Date | null
-
   deletedAt?: Date | null
+}
+
+export type CreateTaskInput = {
+  title: string
+  projectId?: ID | null
+  clientId?: ID | null
+  assigneeId?: ID | null
+  status: TaskStatus
+  priority: TaskPriority
+  description: string
+  dueDate?: Date | null
 }
