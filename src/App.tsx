8379b34ea24ebefc65fd3e7e775c441f88f6f020
Sohail Kachhi <43206109+sohail-iirnt@users.noreply.github.com>
2026-08-24 19:52:12 +0530

    wrap Startup OS with render error recovery boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
+import AppErrorBoundary from './components/system/AppErrorBoundary'
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AppErrorBoundary>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AppErrorBoundary>
   )
 }
 
